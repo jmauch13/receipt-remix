eaf3841cc339533877517e49b6cbab803d973cbb
Jennie Mauch <jenniemauch@me.com>
2026-05-25 12:12:29 -0400

    Fix Render catch all route

--- server.js
+++ server.js
@@ -537,7 +537,7 @@ if (!timedLyrics.length) {
 
 app.use(express.static(path.join(__dirname, "dist")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
